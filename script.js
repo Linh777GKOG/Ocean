ba000e313a3f03d6d93980a1a8f4fdbe4a243e88
Linh Vu <linh777gkog@gmail.com>
2022-06-22 23:09:37 +0700

    check point

--- script.js
+++ script.js
@@ -14,3 +14,45 @@ const trashFormatter = new Intl.NumberFormat('en-us', {
 const MAX_MONEY_RAISED = 30000000;
 
 setupTrash();
+
+async function setupTrash() {
+  const amountRaised = await fetch('https://tscache.com/donation_total.json')
+    .then((res) => res.json())
+    .then((data) => data.count);
+  moneyElem.innerText = currencyFormatter.format(amountRaised);
+
+  const amountLeftToRaise = Math.max(MAX_MONEY_RAISED - amountRaised, 0);
+  const stringifiedAmount = trashFormatter.format(amountLeftToRaise);
+  const trashAmount = {
+    xxl: {
+      amount: parseInt(`${stringifiedAmount[0]}${stringifiedAmount[1]}`),
+      icon: 'bag',
+    },
+    xl: {
+      amount: parseInt(stringifiedAmount[2]),
+      icon: 'takeout',
+    },
+    lg: {
+      amount: parseInt(stringifiedAmount[3]),
+      icon: 'headphones',
+    },
+    md: {
+      amount: parseInt(stringifiedAmount[4]),
+      icon: 'phone',
+    },
+    sm: {
+      amount: parseInt(stringifiedAmount[5]),
+      icon: 'toy-car',
+    },
+    xs: {
+      amount: parseInt(stringifiedAmount[6]),
+      icon: 'bottle',
+    },
+  };
+
+  Object.values(trashAmount).forEach(({ amount, icon }) => {
+    for (let i = 0; i < amount; i++) {
+      createTrash(icon);
+    }
+  });
+}
